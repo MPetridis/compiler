1caa5cf4058920fab86b1e78a44316995738ebd3
MPetridis <m.petridis2002@gmail.com>
2024-09-10 17:06:16 +0300

    lexer and tokens

---
+++ lexer.h
@@ -0,0 +1,64 @@
+#define T_EOF 0
+#define T_TYPEDEF 1
+#define T_CHAR 2
+#define T_INT 3
+#define T_FLOAT 4
+#define T_ENUM 5
+#define T_CLASS 6
+#define T_PRIVATE 7
+#define T_PROTECTED 8
+#define T_PUBLIC 9
+
+#define T_VOID 10
+#define T_STATIC 11
+#define T_CONTINUE 12
+#define T_BREAK 13
+#define T_THIS 14
+#define T_IF 15
+#define T_ELSE 16
+#define T_WHILE 17
+#define T_FOR 18
+#define T_SWITCH 19
+#define T_CASE 20
+#define T_DEFAULT 21
+#define T_RETURN 22
+#define T_CIN 23
+#define T_COUT 24
+#define T_LENGTH 25
+#define T_MAIN 26
+
+#define T_ID 27
+
+#define T_ICONST 28
+#define T_FCONST 29
+#define T_CCONST 30
+
+#define T_OROP 31
+#define T_ANDOP 32
+#define T_EQUOP 33
+#define T_RELOP 34
+#define T_ADDOP 35
+#define T_MULOP 36
+#define T_NOTOP 37
+#define T_INCDEC 38
+#define T_SIZEOP 39
+
+#define T_STRING 40
+
+#define T_LPAREN 41 // (
+#define T_RPAREN 42 // )
+#define T_SEMI 43   // ;
+#define T_DOT 44    // .
+#define T_COMMA 45  // ,
+#define T_ASSIGN 46 // =
+#define T_COLON 47  // :
+#define T_LBRACK 48 // [
+#define T_RBRACK 49 // ]
+#define T_REFER 50
+#define T_LBRACE 51 // {
+#define T_RBRACE 52 // }
+#define T_METH 53
+#define T_INP 54
+#define T_OUT 55
+
+#define T_ERROR -1
